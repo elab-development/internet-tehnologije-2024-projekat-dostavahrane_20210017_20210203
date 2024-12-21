4c395a4ab263d36cebe99d6e91fd49fb58546c25
Anastasija Spasic <anastasijasp2@gmail.com>
2024-12-21 19:27:35 +0100

    Dodate instance za Category i Dish modele preko seeder-a

--- prvidomaci/database/seeders/CategorySeeder.php
+++ prvidomaci/database/seeders/CategorySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
 
 class CategorySeeder extends Seeder
 {
@@ -12,6 +13,58 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        Category::create([
+            'name' => 'Pizza 32cm',
+
+        ]);
+
+        
+        Category::create([
+            'name' => 'Paste',
+
+        ]);
+
+        
+        Category::create([
+            'name' => 'Rostilj',
+
+        ]);
+
+        
+        Category::create([
+            'name' => 'Pizza 50cm',
+
+        ]);
+
+        
+        Category::create([
+            'name' => 'Burgeri',
+
+        ]);
+
+        
+        Category::create([
+            'name' => 'Obrok salate',
+
+        ]);
+
+        
+        Category::create([
+            'name' => 'Sendvici',
+
+        ]);
+
+        
+        Category::create([
+            'name' => 'Pica',
+
+        ]);
+
+        
+        Category::create([
+            'name' => 'Deserti',
+
+        ]);
     }
 }
