dabe99ea55429898ab5f84ed4cb95ed93bafc00a
Anastasija Spasic <anastasijasp2@gmail.com>
2025-01-01 23:00:47 +0100

    Izmene u strukturi modela Dish i Order, kao i odgovarajucih ruta

--- prvidomaci/database/migrations/2024_12_19_205632_create_orders_table.php
+++ prvidomaci/database/migrations/2024_12_19_205632_create_orders_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('restaurant_id');
             $table->unsignedInteger('total_price');
             $table->enum('status', ['pending', 'preparing', 'on_the_way', 'delivered', 'cancelled'])
                   ->default('pending');
